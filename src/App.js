ed742b86a11f5c2d1340f7287cf4c4c9ea328d51
이름 <>
2024-04-14 20:25:06 +0900

    movie app 0414

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello~~</h1>
+      <Food fav="김치" />
+      <Food fav="주꾸미" />
+      <Food fav="삼겹살" />
     </div>
   );
 }
